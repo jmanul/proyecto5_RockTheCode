80b50b7673d174f2223422b96c75eabb8b54dcac
José Manuel Sánchez <jmanul77@gmail.com>
2024-07-13 21:12:42 +0200

    print color gamer in table

--- proyecto5_RockTheCode/public/src/pages/tres/tres.js
+++ proyecto5_RockTheCode/public/src/pages/tres/tres.js
@@ -1,9 +1,43 @@
 import { createSection } from '../../components/section/section';
 import './tres.css'
 
+const control = (numero) => {
+
+     const numberCasilla = document.querySelector(`#casilla-${numero}`);
+     
+     if (numberCasilla.className.includes('ocupada')) {
+          
+          console.log(classList);
+          return; 
+
+     } else {
+          
+          numberCasilla.classList.add('ocupada');
+          const ficha = document.createElement('img');
+          ficha.src = './src/assets/yellow.svg';
+          numberCasilla.append(ficha);
+
+     }
+     
+
+     
+}
+
 export const createTres = (button) => {
 
      createSection(button,'Tres en Raya', '1');
      const containerGame = document.querySelector('.container-game');
-     containerGame.innerText = 'Tres en Raya';
+     const tablero = document.createElement('div');
+     tablero.className = 'tablero';
+     containerGame.append(tablero);
+
+     for (let i = 0; i < 9; i++) {
+
+          const casilla = document.createElement('div');
+          casilla.id = `casilla-${i}`;
+          casilla.classList.add('flex-container', 'casilla');
+          tablero.append(casilla);
+          casilla.addEventListener('click', () => control(i));
+          
+     }
 }
