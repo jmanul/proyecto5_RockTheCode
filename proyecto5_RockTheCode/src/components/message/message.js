66d7016a3a5cdc9a2c3fed9cb2c47513ebed71fe
José Manuel Sánchez <jmanul77@gmail.com>
2024-08-01 01:10:39 +0200

    assets svgs

--- proyecto5_RockTheCode/src/components/message/message.js
+++ proyecto5_RockTheCode/src/components/message/message.js
@@ -5,7 +5,7 @@ export const createMessage = (site, message) => {
      const containerMessage = document.createElement('div');
      containerMessage.classList.add('flex-container', 'container-message');
      containerMessage.id = 'container-message';
-     containerMessage.innerHTML = `<div class="flex-container"><img src="./src/assets/clear.svg" id="clear-message"/><p>${message}</p></div>`;
+     containerMessage.innerHTML = `<div class="flex-container"><img src="./assets/clear.svg" id="clear-message"/><p>${message}</p></div>`;
      site.append(containerMessage);
 
      const clearMessage = document.querySelector('#clear-message').addEventListener('click', () => { 
